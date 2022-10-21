25eb002f52ee1ce2a3ce92f6ac4e0a4e6735c4e7
bram <dasuqiibrohim@gmail.com>
2022-10-21 09:46:41 +0700

    Commit first project.

---
+++ eFishery-Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  eFishery-Test
+//
+//  Created by ACI 2 on 21/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ eFishery-Test/eFishery_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  eFishery_TestApp.swift
+//  eFishery-Test
+//
+//  Created by ACI 2 on 21/10/22.
+//
+
+import SwiftUI
+
+@main
+struct eFishery_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
